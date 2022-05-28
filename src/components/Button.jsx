de90c0ced7b797cd88b7fd1d994775d4eb30ac14
Shekinah <shekinahnzinga@gmail.com>
2022-05-28 18:17:10 +0100

    wip(Ecommerce): trying to fit the button width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function Button() {
+function Button({ color, borderRadius, bgColor, text, size }) {
   return (
-    <div>Button</div>
+    <div
+      type="button"
+      style={{ backgroundColor: bgColor, borderRadius, color }}
+      className={`text-${size} p-3 hover:drop-shadow-xl w-fit`}
+    >
+      {text}
+    </div>
   );
 }
 
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, ecomPieChartData, SparklineAreaData } from '../data/dummy';
+import { ContextProvider } from '../context/ContextProvider';
 
 const Ecommerce = () => (
-  <div>Ecommerce</div>
+  <div className="mt-12">
+    <div className="flex flex-wrap lg:flex-nowrap justify-center">
+      <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <div className="flex justify-between items-center">
+          <div className="">
+            <p className="font-bold text-gray-400">Gains actuels</p>
+            <p className="text-2xl">$67,8745.56</p>
+          </div>
+        </div>
+        <div className="mt-6">
+          <Button size="base" color="white" borderRadius="10px" bgColor="blue" text="Télécharger" />
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Ecommerce;
